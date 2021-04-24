c445e249e7aee7b5d5f86125d0e4992e321efbda
AkiraTerui <hanabi0111@gmail.com>
2021-04-23 21:20:13 -0400

    edits: datasort

--- datasort_mrdata.R
+++ datasort_mrdata.R
@@ -10,33 +10,40 @@
 #' ---
 
 #' # Session information
+  
   sessionInfo()
 
 #' # Load library
 #' Initialize and load `tidyverse`
+  
   # Reset all
   rm(list=ls(all.names=T))
   library(tidyverse)
   
 #' # Read data
+  
   # Data load ----
-  d0 <- read.csv("data/original-mr-data.csv")
-  dat = d0 %>%
+  
+  d0 <- read_csv("data/original-mr-data.csv")
+  dat <- d0 %>%
     mutate(id = as.character(id),
-           SecUL = ceiling(distance*0.1)*10, # upstream landmark
+           SecUL = ceiling(distance * 0.1) * 10, # upstream landmark
            SecDL = SecUL - 10, # downstream landmark
+           date = paste(year, month, day, sep = "-"),
            ym = format(as.Date(date), format="%Y-%m"), # reformat date
-           Julian = julian(as.Date(date)) )# Julian date
-  str(dat)
+           Julian = julian(as.Date(date))) %>% # Julian date
+    select(-X1, -X.1, -X, -No., -No)
 
 #' # Data removal
 #' 
 #' - Remove: ID of individuals that were captured once only at the last occasion
 #' - Remove: individuals with mutiple treatment assignments
+  
   # Data removal ----
+  
   ## Remove: ID of individuals that were captured once only at the last occasion
-  ## Remove: individuals with mutiple treatment assignments
-  dat1 = dat %>%
+  ## Remove: individuals with multiple treatment assignments
+  dat1 <- dat %>%
     drop_na(cluster) %>%
     filter(id %in% names(which(table(id) > 1)) | ym != '2017-10') %>%
     filter(id %in% names(which(table(unlist(tapply(id, treatment, unique))) == 1) ) )
@@ -48,54 +55,94 @@
 #'     - `Occasion` - date (`ym`) transformation: `1...7 = 2016-05...2017-10`
 #'     - `CL` - `cluster` transformation: `c(1, 2, 3) = c('1st', '2nd', '3rd')`
 #'     - `TR` - `treatment` transformation: `c(1, 2, 3) = c('control', 'early', 'late')`
+  
   # History matrix ----
+  
   ## CH: Capture history - binary information of recaptured or not
-  CH = dat1 %>%
-    mutate(Occasion = as.numeric(factor(ym) ), capture = 1) %>%
+  CH <- dat1 %>%
+    mutate(Occasion = as.numeric(factor(ym)),
+           capture = 1) %>%
     arrange(Occasion) %>%
-    pivot_wider(names_from = Occasion, values_from = capture,
-                id_cols = c(id, cluster, treatment), values_fill = list(capture = 0)) %>%
+    pivot_wider(names_from = Occasion,
+                values_from = capture,
+                id_cols = c(id, cluster, treatment),
+                values_fill = list(capture = 0)) %>%
     rename(CL = cluster) %>%
-    mutate(CL = as.numeric(CL)) %>%
+    mutate(CL = as.numeric(factor(CL),
+                           levels = c("1st",
+                                      "2nd",
+                                      "3rd")
+                           )
+           ) %>%
     rename(TR = treatment) %>%
-    mutate(TR = as.numeric(TR)) %>% # 1: control, 2: early, 3: late
+    mutate(TR = as.numeric(factor(TR,
+                                  levels = c("control",
+                                             "early",
+                                             "late")
+                                  )
+                           )
+           ) %>% # 1: control, 2: early, 3: late
     arrange(id)
-
-  ch = CH %>% select(as.character(1:7))
-  for(i in 1:nrow(ch)){
+  
+  ## put NA until the first capture
+  ch <- CH %>% select(as.character(1:7))
+  
+  for(i in 1:nrow(ch)) {
     x <- min(which(ch[i,] == 1)) - 1
-    if(x != 0) ch[i,1:x] <- NA
+    if(x != 0) ch[i, 1:x] <- NA
   }
   
-  CH[,which(colnames(CH) %in% as.character(1:7))] <- ch
+  CH[, which(colnames(CH) %in% as.character(1:7))] <- ch
   
-  CH
+  print(CH)
   
   ## DH: Location history - distance from the downstream end to the upstream landmark of a (re)capture subsection
-  DH = dat1 %>%
-    mutate(Occasion = as.numeric(factor(ym) )) %>%
+  DH <- dat1 %>%
+    mutate(Occasion = as.numeric(factor(ym))) %>%
     arrange(Occasion) %>%
     pivot_wider(names_from = Occasion, values_from = SecUL, id_cols = c(id, cluster, treatment)) %>%
     rename(CL = cluster) %>%
-    mutate(CL = as.numeric(CL)) %>%
+    mutate(CL = as.numeric(factor(CL),
+                           levels = c("1st",
+                                      "2nd",
+                                      "3rd")
+                           )
+           ) %>%
     rename(TR = treatment) %>%
-    mutate(TR = as.numeric(TR)) %>% # 1: control, 2: early, 3: late
+    mutate(TR = as.numeric(factor(TR,
+                                  levels = c("control",
+                                             "early",
+                                             "late")
+                                  )
+                           )
+           ) %>% # 1: control, 2: early, 3: late
     arrange(id)
   
-  DH
+  print(DH)
   
   ## JH: Julian history - Julian date of capture for each occasion
-  JH = dat1 %>%
-    mutate(Occasion = as.numeric(factor(ym) )) %>%
+  JH <- dat1 %>%
+    mutate(Occasion = as.numeric(factor(ym))) %>%
     arrange(Occasion) %>%
     pivot_wider(names_from = Occasion, values_from = Julian, id_cols = c(id, cluster, treatment)) %>%
     rename(CL = cluster) %>%
-    mutate(CL = as.numeric(CL)) %>%
+    mutate(CL = as.numeric(factor(CL),
+                           levels = c("1st",
+                                      "2nd",
+                                      "3rd")
+                           )
+           ) %>%
     rename(TR = treatment) %>%
-    mutate(TR = as.numeric(TR)) %>% # 1: control, 2: early, 3: late
+    mutate(TR = as.numeric(factor(TR,
+                                  levels = c("control",
+                                             "early",
+                                             "late")
+                                  )
+                           )
+           ) %>% # 1: control, 2: early, 3: late
     arrange(id)
   
-  JH
+  print(JH)
 
 #' # Save files
 #' Save matrices
