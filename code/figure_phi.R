89e95513668f8381c5b60375bbd4e3e938405000
aterui <hanabi0111@gmail.com>
2023-10-04 15:30:38 -0400

    edit: update figures

--- code/figure_phi.R
+++ code/figure_phi.R
@@ -8,7 +8,9 @@ d0 <- readRDS("result/re_model_cjs.rds") %>%
   as_tibble(rownames = "param") %>% 
   rename(lower = '2.5%',
          median = '50%',
-         upper = '97.5%')
+         upper = '97.5%') %>%  
+  mutate(x = str_extract(param, pattern = "\\[.{1,}\\]"),
+         y = str_remove_all(x, pattern = "\\[|\\]"))
 
 ## group match
 ## CL: cluster - 1st, 2nd, 3rd = c(1,2,3)
@@ -28,16 +30,16 @@ dat_date <- tibble(occasion = 1:length(date),
 
 # format ------------------------------------------------------------------
 
-dat <- d0 %>% 
+df_phi <- d0 %>% 
   filter(str_detect(param, "phi")) %>% 
-  mutate(x = str_extract(param, pattern = "\\[.{1,}\\]"),
-         y = str_remove_all(x, pattern = "\\[|\\]")) %>%
-  separate(y, into = c("group", "occasion"), sep = ",") %>% 
+  separate(y,
+           into = c("group", "occasion"),
+           sep = ",",
+           fill = "left") %>% 
   mutate(group = as.numeric(group),
          occasion = as.numeric(occasion)) %>% 
   left_join(Grid, by = c("group" = "G")) %>% 
   left_join(dat_date, by = "occasion") %>% 
-  filter(occasion == 7) %>% 
   mutate(treatment = case_when(TR == 1 ~ "Control",
                                TR == 2 ~ "Early",
                                TR == 3 ~ "Late"),
@@ -45,12 +47,34 @@ dat <- d0 %>%
                                  c("Early", "Late")),
          cluster = case_when(CL == 1 ~ "Fast",
                              CL == 2 ~ "Medium",
-                             CL == 3 ~ "Slow"))
+                             CL == 3 ~ "Slow"),
+         cluster = fct_relevel(cluster,
+                               c("Slow", "Medium"))) %>% 
+  filter(occasion == 7)
+
+df_lrr <- d0 %>% 
+  filter(str_detect(param, "lrr")) %>% 
+  separate(y,
+           into = c("CL", "TR"),
+           sep = ",",
+           fill = "left",
+           convert = TRUE) %>% 
+  left_join(Grid, by = c("CL", "TR")) %>% 
+  mutate(treatment = case_when(TR == 1 ~ "Control",
+                               TR == 2 ~ "Early",
+                               TR == 3 ~ "Late"),
+         treatment = fct_relevel(treatment,
+                                 c("Early", "Late")),
+         cluster = case_when(CL == 1 ~ "Fast",
+                             CL == 2 ~ "Medium",
+                             CL == 3 ~ "Slow"),
+         cluster = fct_relevel(cluster,
+                               c("Slow", "Medium")))
 
 
 # plot --------------------------------------------------------------------
 
-g <- ggplot(dat) +
+g_phi <- ggplot(df_phi) +
   geom_segment(aes(x = cluster,
                    xend = cluster,
                    y = lower,
@@ -63,18 +87,45 @@ g <- ggplot(dat) +
   xlab("Growth cluster") +
   theme_bw() +
   theme(strip.background = element_blank(),
-        panel.background = element_rect(grey(0.95)),
+        panel.background = element_rect("white"),
         panel.grid = element_blank())
 
-print(g)  
+g_lrr <- ggplot(df_lrr) +
+  geom_segment(aes(x = cluster,
+                   xend = cluster,
+                   y = lower,
+                   yend = upper),
+               color = grey(0.7)) +
+  geom_point(aes(x = cluster, y = median), 
+             size = 2) +
+  facet_wrap(facets = ~ treatment, ncol = 3) + 
+  ylab("LRR Survival Probability") +
+  xlab("Growth cluster") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  theme_bw() +
+  theme(strip.background = element_blank(),
+        panel.background = element_rect("white"),
+        panel.grid = element_blank())
 
-ggsave(g,
+
+ggsave(g_phi,
        file = "output/figure_phi.eps",
        width = 8,
        height = 3,
        device = "eps")
 
-ggsave(g,
+ggsave(g_phi,
        file = "output/figure_phi.pdf",
+       width = 8,
+       height = 3)
+
+ggsave(g_lrr,
+       file = "output/figure_lrr.eps",
+       width = 8,
+       height = 3,
+       device = "eps")
+
+ggsave(g_lrr,
+       file = "output/figure_lrr.pdf",
        width = 8,
        height = 3)
